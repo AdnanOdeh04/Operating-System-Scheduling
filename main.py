ce99fafe7880716c743dc206269655d5c41d5fdb
Adnan Odeh <68065743+AdnanOdeh04@users.noreply.github.com>
2024-12-29 15:20:25 +0200

    3:20PM, 29-12-2024

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pandas as pd
+import test
 
 data = pd.read_csv("./data.csv", delimiter="|")
 #index here for the dataFrame (data) which is started with value zero to track all elements in the dataFrame
 #iterrow() to move through all rows in the data
+data_in_dictionary = {}
 for index,row in data.iterrows():
-    print(f"{row['PID']}, {row['Arrival Time']}, {row['Priority']}, {row['Sequence of CPU and IO bursts']}")
+    data_in_dictionary[row['PID']] = test.Data(row['PID'], row['Arrival Time'], row['Priority'], row['Sequence of CPU and IO bursts'])
+    #print(f"{row['PID']}, {row['Arrival Time']}, {row['Priority']}, {row['Sequence of CPU and IO bursts']}")
+    print(data_in_dictionary[row['PID']].calculate_total_burst())
 
 
 
--- test.py
+++ test.py
@@ -1,18 +1,40 @@
+import re
+import numpy as np
+
+
 class Data:
     PID = 0
     Arrival_Time = 0
     Priority = 0
     Sequence_of_CPU_and_IO_bursts = []
-    Check=0
+    Check = 0
 
-    def __init__(self, PID, Arrival_Time, Priority, Sequence_of_CPU_and_IO_bursts, Check):
+    def __init__(self, PID, Arrival_Time, Priority, Sequence_of_CPU_and_IO_bursts, Check=0):
         self.PID = PID
         self.Arrival_Time = Arrival_Time
         self.Priority = Priority
         self.Sequence_of_CPU_and_IO_bursts = Sequence_of_CPU_and_IO_bursts
         self.Check = Check
 
+    def printData(self):
+        print(
+            f"{self.PID}, {self.Arrival_Time}, {self.Priority}, {self.Sequence_of_CPU_and_IO_bursts}, Check->{self.Check}")
 
+    def calculate_total_burst(self):
+        newList = self.Sequence_of_CPU_and_IO_bursts.split()
+        list_total_Bursts = []
+        list_all_Bursts = []
+        sum = 0
+        for i in newList:
+            x = re.search(r"{.*}", i)
+            val = list(x.span())
+            new = (i[val[0] + 1:val[1] - 1]).split(",")
+            list_total_Bursts = [c for c in new if c.isdigit()]
+            npList = np.array(list_total_Bursts, dtype=int)
+            sum = np.sum(npList)
+            y = re.search(r".*{", i)
+            valType = list(y.span())
+            Type = i[valType[0]:valType[1] - 1]
 
-test1 = Data(1, 1, 10, [['C', 'R(1)', 50, 'F(1)']])
-print(test1.Sequence_of_CPU_and_IO_bursts)
+            list_all_Bursts.append([Type,int(sum)])
+        return list_all_Bursts
