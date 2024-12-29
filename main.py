34456524c2003d0859a8937f39d21ee183eebc33
Adnan Odeh <68065743+AdnanOdeh04@users.noreply.github.com>
2024-12-29 13:17:00 +0200

    1:16 PM, 29-12-2024

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Alaa is piece of shit")
+import pandas as pd
+
+data = pd.read_csv("./data.csv", delimiter="|")
+#index here for the dataFrame (data) which is started with value zero to track all elements in the dataFrame
+#iterrow() to move through all rows in the data
+for index,row in data.iterrows():
+    print(f"{row['PID']}, {row['Arrival Time']}, {row['Priority']}, {row['Sequence of CPU and IO bursts']}")
+
+
+
